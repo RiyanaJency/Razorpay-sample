c2835dcd7e114097a4fda9ef6e8b65502edefa47
RiyanaJency <riyanajency123@gmail.com>
2024-11-12 20:15:48 +0530

    newcommit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import logo from './logo.svg';
 import { BrowserRouter as Router, Routes,Route } from 'react-router-dom';
 import './App.css';
@@ -8,23 +7,14 @@ import Payment from './Component/Payment';
 import Home from './Component/Home';
 import UserProfile from './Component/User profile';
 import Betting from './Component/Betting';
-=======
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css';
-import Navbar from './Navbar';
-import Bet from './betting ';
-import Footer from './Footer';
-import Userprofile from './Userprofile';
-import Home from './Home';
->>>>>>> b95f8af33058c5917b0b0bff65a0c15c76eb801f
+
 
 
 function App() {
   return (
     <>
     
-<<<<<<< HEAD
+
   <Router>
   <NavigationBar />
     <Routes>
@@ -38,32 +28,8 @@ function App() {
   </Router>
   <Footer/>
   </>
-=======
-    <Router>
-      <Navbar/>
-      <Routes>
-        <Route path="/" element={<Home/>} />
 
-        <Route path="betting" element={< Bet/>} />
-        <Route path="Userprofile" element={< Userprofile/>} />
-        <Route path="Home" element={< Home/>} />
-
-
-
-
-        
-
-
-
-
-      </Routes>
-    </Router>
-    <Footer/>
-    </>
->>>>>>> b95f8af33058c5917b0b0bff65a0c15c76eb801f
-  );
+   );
 }
 
 export default App;
-
-
